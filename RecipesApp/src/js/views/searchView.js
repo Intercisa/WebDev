ca21e8b84a70678bf74e2f539029c64985d140f0
Intercisa <sipiczkibarna@gmail.com>
2020-06-07 16:44:40 +0200

    some changes

--- RecipesApp/src/js/views/searchView.js
+++ RecipesApp/src/js/views/searchView.js
@@ -9,6 +9,23 @@ export const clearResults = () =>{
     elements.searchResList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if(title.length > limit){
+        title.split(' ').reduce((acc, curr)=>{
+            if(acc + curr.length <= limit){
+                newTitle.push(curr);
+            }
+            return acc + curr.length; 
+        }, 0);
+
+        //return the actual result 
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+}
+
+
 const renderRecipe =  recipe => {
     const markup = `
             <li>
@@ -17,7 +34,7 @@ const renderRecipe =  recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
