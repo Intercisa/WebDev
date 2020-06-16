33131789983ead74b53105e4f8a23d20dc853bcd
Intercisa <sipiczkibarna@gmail.com>
2020-06-16 17:38:29 +0200

    lazy

--- RecipesApp/src/js/index.js
+++ RecipesApp/src/js/index.js
@@ -15,7 +15,6 @@ import { elements, renderLoader, clearLoader } from './views/base';
 * - Liked recipes
 */
 const state = {};
-window.state = state; //TESTING
 
 
 /**
@@ -154,7 +153,6 @@ elements.shopping.addEventListener('click', e => {
  * 
  * **/
 
-state.likes = new Likes(); //TESTING
 const controlLike = () =>{
     if(!state.likes) state.likes = new Likes();
     const currentID = state.recipe.id;
@@ -188,9 +186,19 @@ const controlLike = () =>{
     likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
 
+//restore liked recipes on page load
+window.addEventListener('load', ()=>{
+    state.likes = new Likes(); 
+    
+    //Restore likes
+    state.likes.readStorage();
+    
+    //toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
 
-
-
+    //render the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 //Handling recipe button clicks
 elements.recipe.addEventListener('click', e =>{
@@ -213,9 +221,6 @@ elements.recipe.addEventListener('click', e =>{
         //like controller 
         controlLike();
     }
-
-
-
     
 });
 
